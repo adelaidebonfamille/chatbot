14bc71781ba288125171ed3bba09dd65677eec33
adelaidebonfamille <164980549+adelaidebonfamille@users.noreply.github.com>
2024-05-29 09:08:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Chatbot</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css" />
+    <script src="response.js" defer></script>
+    <script src="app.js" defer></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="chat-header">
+        <div class="logo">
+          <img src="images/mariearistocats3.jpg" alt="cwt" />
+        </div>
+        <div class="title">Chat Bot</div>
+      </div>
+      <div class="chat-body"></div>
+      <div class="chat-input">
+        <div class="input-sec">
+          <input type="text" id="txtInput" placeholder="Kirim Pesan" autofocus />
+        </div>
+        <div class="send">
+          <img src="images/send.png" alt="send" />
+        </div>
+      </div>
+    </div>
+    
+  </body>
+</html>
